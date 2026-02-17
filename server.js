e5c303a260666b9e81dc59ad3444550591904206
Hasaan Developer <muhammedhasaan09@gmail.com>
2026-02-17 21:30:59 +0500

    update

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ connectDB()
 
 // Middleware
 app.use(cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000","https://www.quettaalamgirhotelbranch3.com" , "https://pos-fruntend.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
